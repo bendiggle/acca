c5b29d10304d76e7ab4226e0fa74efcb128e38a3
Ben Diggle <ben.diggle5@gmail.com>
2018-04-17 19:41:58 +0100

    Add hover background-color of buttons. Add fixed pos of menu (#8)
    Add hover background colour for menu links
    Add fixed pos to menu for when scrolling down the page

--- app/src/components/header.jsx
+++ app/src/components/header.jsx
@@ -4,10 +4,12 @@ import Navigation from './navigation';
 const Header = () => {
     return (
         <header>
-            <div id="logo">
-                <h1>Bet Net</h1>
+            <div id="header">
+                <div id="logo">
+                    <h1>Bet Net</h1>
+                </div>
+                <Navigation/>
             </div>
-            <Navigation/>
         </header>
     )
 };
