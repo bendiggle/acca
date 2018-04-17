3405670aadbb350b6c8e661bfe2cf6c3e66b262a
Ben Diggle <ben.diggle5@gmail.com>
2018-04-17 16:29:21 +0100

    Add mock data and add post to home component (#6)
    Add mock data json file
    Modify home component to a class and display the postData
    Add sass styling

--- app/src/components/home.jsx
+++ app/src/components/home.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import data from '../../../mock-data.json';
 
-const Home = () => {
-    return (
-        <main className="">
-            <h1>Home</h1>
-            <p>
-                EXAMPLE
-            </p>
-        </main>
-    );
-};
+class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = { postData };
+    }
+
+    displayPost(data) {
+        return (
+            <div className="post" key={data.postId}>
+                <p id="username">{data.username}</p>
+                <p>{data.post}</p>
+            </div>
+        );
+    }
+
+    render() {
+
+        const posts = this.state.postData.data.map(post => {
+            return this.displayPost(post);
+        });
+
+        return (
+            <main className="">
+                <h1>Home</h1>
+                <div>{posts}</div>
+            </main>
+        );
+    }
+}
 
 export default Home;
